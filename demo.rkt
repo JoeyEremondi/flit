cbedb5856961a1fdc0eb06b2ffdcb5ebbfdbdf2c
Joey Eremondi <joey@eremondi.com>
2024-10-17 00:15:48 -0600

    First version of working holes

---
+++ demo.rkt
@@ -0,0 +1,24 @@
+#lang s-exp "main.rkt"
+
+(define-type Foo
+  [Bar (x : Number)]
+  [Baz (y : Boolean)])
+
+;(define (map (f : ('a -> 'b)) (lst : (Listof 'a))) : (Listof 'b)
+;  (TODO))
+
+(define (f [x : 'b] [y : Foo]) : Number
+  (+ (if #t 3 ((TODO) y)) 3))
+
+;(define zz : Boolean TODO)
+
+;(TODO "write more code!")
+
+;(let ((x 1) (y 2)) (TODO/bindings))
+
+;(with-command 444)
+
+;; The entire definition containing the TODO is highlighted as a thing to be done now.
+;(define/todos incomplete-fun
+;  (lambda (y)
+;    (inner-TODO "This function is incomplete")))
